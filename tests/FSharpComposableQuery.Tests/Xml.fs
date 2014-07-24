f291085f4cb3207737371688d80f0e39fd1d0a15
ixtreon <ixtreon@gmail.com>
2014-07-24 12:46:12 +0100

    Updated comments and style of Xml.fs

--- tests/FSharpComposableQuery.Tests/Xml.fs
+++ tests/FSharpComposableQuery.Tests/Xml.fs
@@ -10,7 +10,7 @@ open FSharpComposableQuery
 /// <summary>
 /// Contains example queries and operations on the Xml database. 
 /// The queries here are further wrapped in quotations to allow for their evaluation in different contexts (see Utils.fs).  
-/// <para>These tests require the schema from sql/xml.sql in a database referred to in app.config </para>
+/// <para> These tests require the schema from sql/xml.sql in a database referred to in app.config </para>
 /// </summary>
 module Xml =
     [<Literal>]
@@ -43,25 +43,72 @@ module Xml =
         | Name of string
         | Filter of Path
 
+        static member Child = Axis Child
+        static member Self = Axis Self
+        static member Descendant = Axis Descendant
+        static member DescendantOrSelf = Axis DescendantOrSelf
+        static member Parent = Axis(Rev Child)
+        static member Ancestor = Axis(Rev Descendant)
+        static member AncestorOrSelf = Axis(Rev DescendantOrSelf)
+        static member Following = Axis Following
+        static member FollowingSibling = Axis FollowingSibling
+        static member Preceding = Axis(Rev Following)
+        static member PrecedingSibling = Axis(Rev FollowingSibling)
+
+        /// <summary>
+        /// Indicates the concatenation of two paths. 
+        /// </summary>
+        /// <param name="p1">The first path. </param>
+        /// <param name="p2">The second path. </param>
+        static member (/) (p1, p2) = Seq(p1, p2)
+
+        /// <summary>
+        /// Indicates the concatenation of a path with an element name filter. 
+        /// </summary>
+        /// <param name="path">The initial path. </param>
+        /// <param name="name">The name filter for the element. </param>
+        static member (%) (path, name) = Seq(path, Name name)
+
+        /// <summary>
+        /// Indicates the concatenation of a path with a path filter. 
+        /// </summary>
+        /// <param name="path1">The initial path. </param>
+        /// <param name="path2">The path filter. </param>
+        static member (^^) (path1, path2) = Seq(path1, Filter(path2))
+
     let internal db = schema.GetDataContext()
     let internal data = db.Data
     let internal text = db.Text
     let internal attributes = db.Attribute
 
     // XML document loading/shredding
-    let mutable idx = 0
 
+    let mutable idx = 0
     let new_id() =
         let i = idx
         idx <- i + 1
         i
 
+    // Walks the XML document
     let rec traverseXml entry parent i (node : XNode) =
+        // creates an attribute record
+        let traverseAttribute entry parent (att : XAttribute) =
+            let a = new Attribute()
+            a.Element <- parent
+            a.Name <- att.Name.LocalName
+            a.Value <- att.Value
+            attributes.InsertOnSubmit(a)
+
+        // recursively traverse all child nodes
+        let traverseChildren entry parent i (xmls) = 
+            Seq.fold (traverseXml entry parent) i xmls
+
         match node with
         | :? XElement as xml ->
             let id = new_id()
             let j = Seq.iter (traverseAttribute entry id) (xml.Attributes())
             let j = traverseChildren entry id (i + 1) (xml.Nodes())
+
             let d = new Data()
             d.Name <- xml.Name.ToString()
             d.ID <- id
@@ -70,10 +117,12 @@ module Xml =
             d.Post <- j
             d.Parent <- parent
             data.InsertOnSubmit(d)
+
             data.Context.SubmitChanges()
             j + 1
         | :? XText as xtext ->
             let id = new_id()
+
             let d = new Data()
             d.Name <- "#text"
             d.ID <- id
@@ -82,23 +131,20 @@ module Xml =
             d.Post <- i
             d.Parent <- parent
             data.InsertOnSubmit(d)
-            data.Context.SubmitChanges()
+
             let t = new Text()
             t.ID <- id
             t.Value <- xtext.Value
             text.InsertOnSubmit(t)
+
+            data.Context.SubmitChanges()
             i + 1
         | _ -> i
-
-    and traverseChildren entry parent i (xmls) = Seq.fold (traverseXml entry parent) i xmls
-
-    and traverseAttribute entry parent att =
-        let a = new Attribute()
-        a.Element <- parent
-        a.Name <- att.Name.LocalName
-        a.Value <- att.Value
-        attributes.InsertOnSubmit(a)
-
+        
+    /// <summary>
+    /// Parses the given file as an Xml document, and then inserts its contents in the database. 
+    /// </summary>
+    /// <param name="filename">The path to the Xml file to be loaded. </param>
     let insertXml entry xml =
         let root_id = new_id()
         let j = traverseXml entry root_id 1 xml
@@ -112,48 +158,50 @@ module Xml =
         data.InsertOnSubmit(d)
         data.Context.SubmitChanges()
 
+    /// <summary>
+    /// Parses the given file as an Xml document, and then inserts its contents in the database. 
+    /// </summary>
+    /// <param name="filename">The path to the Xml file to be loaded. </param>
     let loadXml entry (filename : string) =
         let xml = XElement.Load(filename)
         insertXml entry xml
 
-    // Clears all relevant tables in the database. 
+    /// <summary>
+    /// Clears all relevant tables in the database. 
+    /// </summary>
     let dropTables() =
         ignore (db.DataContext.ExecuteCommand("TRUNCATE TABLE [MyXml].[dbo].[Attribute]"))
         ignore (db.DataContext.ExecuteCommand("TRUNCATE TABLE [MyXml].[dbo].[Text]"))
         ignore (db.DataContext.ExecuteCommand("TRUNCATE TABLE [MyXml].[dbo].[Data]"))
 
+    /// <summary>
+    /// Loads the basicXml file
+    /// </summary>
     let loadBasicXml() =
         dropTables()
         insertXml 0 basicXml
 
-    let rec internal axisPred' axis =
-        match axis with
-        | Self -> <@ fun (row1 : Data) (row2 : Data) -> row1.ID = row2.ID @>
-        | Child -> <@ fun (row1 : Data) (row2 : Data) -> row1.ID = row2.Parent @>
-        | Descendant -> <@ fun (row1 : Data) (row2 : Data) -> row1.Pre < row2.Pre && row2.Post < row1.Post @>
-        | DescendantOrSelf -> <@ fun (row1 : Data) (row2 : Data) -> row1.Pre <= row2.Pre && row2.Post <= row1.Post @>
-        | Following -> <@ fun (row1 : Data) (row2 : Data) -> row1.Post < row2.Pre @>
-        | FollowingSibling -> <@ fun (row1 : Data) (row2 : Data) -> row1.Post < row2.Pre && row1.Parent = row2.Parent @>
-        | Rev axis -> <@ fun row1 row2 -> (%axisPred' axis) row2 row1 @>
-
+    /// <summary>
+    /// Returns an expression testing whether two rows match the specified axis predicate. 
+    /// </summary>
+    /// <param name="axis">The axis predicate to test the rows against. </param>
     let internal axisPred axis =
-        <@ fun (row1 : Data) (row2 : Data) -> row1.Entry = row2.Entry && (%(axisPred' axis)) row1 row2 @>
+        let rec axisPredRec axis =
+            match axis with
+            | Self -> <@ fun (row1 : Data) (row2 : Data) -> row1.ID = row2.ID @>
+            | Child -> <@ fun (row1 : Data) (row2 : Data) -> row1.ID = row2.Parent @>
+            | Descendant -> <@ fun (row1 : Data) (row2 : Data) -> row1.Pre < row2.Pre && row2.Post < row1.Post @>
+            | DescendantOrSelf -> <@ fun (row1 : Data) (row2 : Data) -> row1.Pre <= row2.Pre && row2.Post <= row1.Post @>
+            | Following -> <@ fun (row1 : Data) (row2 : Data) -> row1.Post < row2.Pre @>
+            | FollowingSibling -> <@ fun (row1 : Data) (row2 : Data) -> row1.Post < row2.Pre && row1.Parent = row2.Parent @>
+            | Rev axis -> <@ fun row1 row2 -> (%axisPredRec axis) row2 row1 @>
+        <@ fun (row1 : Data) (row2 : Data) -> row1.Entry = row2.Entry && (%(axisPredRec axis)) row1 row2 @>
 
-    let (./.) p1 p2 = Seq(p1, p2)
-    let child = Axis Child
-    let self = Axis Self
-    let descendant = Axis Descendant
-    let descendant_or_self = Axis DescendantOrSelf
-    let parent = Axis(Rev Child)
-    let ancestor = Axis(Rev Descendant)
-    let ancestor_or_self = Axis(Rev DescendantOrSelf)
-    let following = Axis Following
-    let followingsibling = Axis FollowingSibling
-    let preceding = Axis(Rev Following)
-    let precedingsibling = Axis(Rev FollowingSibling)
-    let (.%.) path name = Seq(path, Name name)
-    let (.^.) path1 path2 = Seq(path1, Filter(path2))
-
+    /// <summary>
+    /// Returns an expression that will execute the given path query on the specified data source, when provided with a root node. 
+    /// </summary>
+    /// <param name="data">The data source. </param>
+    /// <param name="path">The path to run on the data source. </param>
     let internal pathQuery data (path) =
         let rec pathQ path =
             match path with
@@ -191,11 +239,12 @@ module Xml =
                        if (root.Parent = -1 && root.Entry = rootId) then yield row'.ID
            } @>
 
-    
-    let xp0 = child ./. child //                                                        /*/*
-    let xp1 = descendant ./. parent //                                                  //*/parent::*
-    let xp2 = descendant ./. (Filter(followingsibling .%. "dirn")) //                   //*[following-sibling::d]
-    let xp3 = descendant .%. "year" ./. Filter(ancestor ./. preceding .%. "dir") //     //f[ancestor::*/preceding::b]
+
+
+    let xp0 = Path.Child / Path.Child                                                       //  /*/*
+    let xp1 = Path.Descendant / Path.Parent                                                 //  //*/parent::*
+    let xp2 = Path.Descendant / (Filter(Path.FollowingSibling % "dirn"))                    //  //*[following-sibling::d]
+    let xp3 = Path.Descendant % "year" / Filter(Path.Ancestor / Path.Preceding % "dir")     //  //f[ancestor::*/preceding::b]
 
     [<TestFixture>]
     type TestClass() =
