e34797d4169c738feb6a547133f6387d370ac215
Phil de Joux <phil.dejoux@blockscope.com>
2014-11-02 17:11:11 +1300

    #13 FCQ-Brands failing sample.

---
+++ tests/FCQ-Brands/README.md
@@ -0,0 +1,3 @@
+# Setup
+
+In SQL Management Studio or Visual Studio, with SQLCMD mode enable, run the scripts 001-* and 002-* after first creating a database named FCQ-Brands.
---
+++ tests/FCQ-Brands/dbo/Tables/UserName.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[UserName] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [AuthId]   INT           NOT NULL,
+    [UserName] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_UserName] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserName_UserAuth] FOREIGN KEY ([AuthId]) REFERENCES [dbo].[UserAuth] ([Id]) ON DELETE CASCADE
+);
+
+
+
+
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_UserName]
+    ON [dbo].[UserName]([UserName] ASC);
+
+
+
