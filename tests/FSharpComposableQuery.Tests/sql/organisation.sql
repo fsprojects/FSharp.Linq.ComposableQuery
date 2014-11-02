1706939d7573fdef3e017637a264871cff1c5fcc
Phil de Joux <phil.dejoux@blockscope.com>
2014-11-02 16:12:56 +1300

    #11 Use a variable for the database name.

--- tests/FSharpComposableQuery.Tests/sql/organisation.sql
+++ tests/FSharpComposableQuery.Tests/sql/organisation.sql
@@ -6,21 +6,20 @@ GO
 USE [master];
 GO
 
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'organisation')
-                DROP DATABASE organisation;
+:setvar DatabaseName "FCQ-Org"
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = [$(DatabaseName)])
+                DROP DATABASE [$(DatabaseName)];
 GO
 
--- Create the MyPeople database.
-CREATE DATABASE organisation COLLATE SQL_Latin1_General_CP1_CI_AS;
+CREATE DATABASE [$(DatabaseName)] COLLATE SQL_Latin1_General_CP1_CI_AS;
 GO
 
--- Specify a simple recovery model 
--- to keep the log growth to a minimum.
-ALTER DATABASE organisation
-                SET RECOVERY SIMPLE;
+-- Specify a simple recovery model to keep the log growth to a minimum.
+ALTER DATABASE [$(DatabaseName)] SET RECOVERY SIMPLE;
 GO
 
-USE organisation;
+USE [$(DatabaseName)];
 GO
 
 CREATE TABLE [dbo].[Departments] (
--- tests/FSharpComposableQuery.Tests/sql/simple.sql
+++ tests/FSharpComposableQuery.Tests/sql/simple.sql
@@ -6,21 +6,20 @@ GO
 USE [master];
 GO
 
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'MyDatabase')
-                DROP DATABASE MyDatabase;
+:setvar DatabaseName "FCQ-Simple"
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = [$(DatabaseName)])
+                DROP DATABASE [$(DatabaseName)];
 GO
 
--- Create the MyDatabase database.
-CREATE DATABASE MyDatabase COLLATE SQL_Latin1_General_CP1_CI_AS;
+CREATE DATABASE [$(DatabaseName)] COLLATE SQL_Latin1_General_CP1_CI_AS;
 GO
 
--- Specify a simple recovery model 
--- to keep the log growth to a minimum.
-ALTER DATABASE MyDatabase
-                SET RECOVERY SIMPLE;
+-- Specify a simple recovery model to keep the log growth to a minimum.
+ALTER DATABASE [$(DatabaseName)] SET RECOVERY SIMPLE;
 GO
 
-USE MyDatabase;
+USE [$(DatabaseName)];
 GO
 
 CREATE TABLE [dbo].[Course] (
@@ -53,7 +52,6 @@ CREATE TABLE [dbo].[LastStudent] (
 );
 
 -- Insert data into the tables.
-USE MyDatabase
 INSERT INTO Course (CourseID, CourseName)
 VALUES(1, 'Algebra I');
 INSERT INTO Course (CourseID, CourseName)
